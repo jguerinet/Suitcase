6994d90b18d87748762b59a8f45c32f262b0ee03
Julien Guerinet <julien@guerinet.com>
2016-01-27 15:49:43 -0500

    Created the DialogHelper class, and added 2 methods to create neutral alert dialogs.

---
+++ library/src/main/java/com/guerinet/utils/DialogHelper.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Julien Guerinet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.guerinet.utils;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Static helper methods to quickly create {@link AlertDialog}s
+ * @author Julien Guerinet
+ * @since 1.0.0
+ */
+public class DialogHelper {
+
+    /**
+     * Creates an alert dialog with one 'ok' button
+     *
+     * @param context  App context
+     * @param title    Dialog title
+     * @param message  Dialog message
+     * @param listener {@link DialogInterface.OnClickListener} for the button
+     */
+    public static void neutral(Context context, @StringRes int title, @StringRes int message,
+            DialogInterface.OnClickListener listener) {
+        new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setNeutralButton(android.R.string.ok, listener)
+                .show();
+    }
+
+    /**
+     * Creates an alert dialog with one 'ok' button
+     *
+     * @param context App context
+     * @param title   Dialog title
+     * @param message Dialog message
+     */
+    public static void neutral(Context context, @StringRes int title, @StringRes int message) {
+        neutral(context, title, message, null);
+    }
+}
