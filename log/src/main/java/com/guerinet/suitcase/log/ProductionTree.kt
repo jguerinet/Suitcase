72b223a1d6604fdb495d42dd2001e1476c9a72e3
Julien Guerinet <julien@guerinet.com>
2017-05-25 20:31:33 -0400

    Added the Kotlin production tree in the new log module.

--- log/src/main/java/com/guerinet/suitcase/log/ProductionTree.kt
+++ log/src/main/java/com/guerinet/suitcase/log/ProductionTree.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Julien Guerinet
+ * Copyright 2016-2017 Julien Guerinet
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,44 +14,42 @@
  * limitations under the License.
  */
 
-package com.guerinet.utils;
+package com.guerinet.suitcase.log
 
-import android.util.Log;
-
-import timber.log.Timber;
+import android.util.Log
+import timber.log.Timber
 
 /**
- * Timber tree that is used for crash reporting in production
+ * Timber tree used for crash reporting in production
  * @author Julien Guerinet
- * @since 1.0.0
+ * @since 2.0.0
  */
-public abstract class ProductionTree extends Timber.Tree {
+abstract class ProductionTree  : Timber.Tree() {
 
-    @Override
-    protected void log(int priority, String tag, String message, Throwable t) {
+    override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
         // We don't want to log verbose and debug logs
         if (priority == Log.VERBOSE || priority == Log.DEBUG) {
-            return;
+            return
         }
 
-        log(message);
-        // If there's a crash and it's not a socket timeout, log it too
+        if (message != null) {
+            // Log the message if there is one
+            log(message)
+        }
+
         if (t != null) {
-            logException(t);
+            // Log the throwable if there is one
+            logException(t)
         }
     }
 
     /**
-     * Called when there is a message to log
-     *
-     * @param message The message to log
+     * Called when there is a [message] to log
      */
-    protected abstract void log(String message);
+    abstract fun log(message: String)
 
     /**
-     * Called when there is an exception to log
-     *
-     * @param t The {@link Throwable} to log
+     * Called when there is a [Throwable] [t] to log
      */
-    protected abstract void logException(Throwable t);
+    abstract fun logException(t: Throwable)
 }
