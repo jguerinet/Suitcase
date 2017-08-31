0cb49b7f4dd22c2cafe7fe22f8db74f3e4406492
Julien Guerinet <julien@guerinet.com>
2017-08-31 12:23:15 -0400

    Added the empty analytics module.

--- analytics/src/test/java/com/guerinet/suitcase/analytics/ExampleUnitTest.java
+++ analytics/src/test/java/com/guerinet/suitcase/analytics/ExampleUnitTest.java
@@ -14,4 +14,20 @@
  * limitations under the License.
  */
 
-include ':util', ':date', ':prefs', ':log', ':dialog', ':ui', ':io', ':db', ':analytics'
+package com.guerinet.suitcase.analytics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
