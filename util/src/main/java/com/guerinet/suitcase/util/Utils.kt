30cd86cad4d0bc201aff5b347485b99602673405
Julien Guerinet <julien@guerinet.com>
2018-08-09 13:13:32 -0400

    Remove deprecated Utils methods.

--- util/src/main/java/com/guerinet/suitcase/util/Utils.kt
+++ util/src/main/java/com/guerinet/suitcase/util/Utils.kt
@@ -16,24 +16,7 @@
 
 package com.guerinet.suitcase.util
 
-import android.content.ActivityNotFoundException
-import android.content.Context
 import android.content.pm.PackageManager
-import android.net.ConnectivityManager
-import android.net.Uri
-import android.support.annotation.ColorRes
-import android.support.annotation.DrawableRes
-import android.support.annotation.StringRes
-import android.widget.Toast
-import com.guerinet.suitcase.util.extensions.getAttributeResourceId
-import com.guerinet.suitcase.util.extensions.getFile
-import com.guerinet.suitcase.util.extensions.getResourceId
-import com.guerinet.suitcase.util.extensions.isConnected
-import com.guerinet.suitcase.util.extensions.openCustomTab
-import com.guerinet.suitcase.util.extensions.openPdf
-import com.guerinet.suitcase.util.extensions.openPlayStoreApp
-import com.guerinet.suitcase.util.extensions.openUrl
-import java.io.File
 import java.util.*
 
 /**
@@ -47,9 +30,7 @@ object Utils {
      * @return Randomly generated UUID
      */
     @JvmStatic
-    fun uuid():String {
-        return UUID.randomUUID().toString()
-    }
+    fun uuid(): String = UUID.randomUUID().toString()
 
     /**
      * Returns true we have been granted a permission (this is checked using the [grantResults])
@@ -59,8 +40,8 @@ object Utils {
             grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
 
     /**
-     *  @return True if the [newVersionName] is more recent than the [oldVersionName] (which is the
-     *      current version if not specified)
+     * Returns true if the [newVersionName] is more recent than the [oldVersionName] (which is the
+     *  current version if not specified)
      */
     @JvmStatic
     @JvmOverloads
@@ -99,120 +80,4 @@ object Utils {
         // Old version is same as new version: no version necessary
         return false
     }
-
-    /* DEPRECATED */
-
-    /**
-     * Deletes the contents of a [folder] and the folder itself if [deleteFolder] is true
-     */
-    @JvmStatic
-    @JvmOverloads
-    @Deprecated("Replaced with by a Kotlin method", ReplaceWith("folder.deleteRecursively()"))
-    fun deleteFolder(folder: File, deleteFolder: Boolean = true) = folder.deleteRecursively()
-
-    /**
-     * @return Folder with the given [folderName] and from the given [type] from the external
-     *  files directory using the app [context]. Will create it if it doesn't exist already
-     */
-    @JvmStatic
-    @Deprecated("Replaced by extension", ReplaceWith("context.getFile(true, folderName, type)",
-            "com.guerinet.suitcase.util.extensions.getFile"))
-    fun getFolder(context: Context, folderName: String, type: String?): File =
-            context.getFile(true, folderName, type)
-
-    /**
-     * @param manager ConnectivityManager instance
-     * @return True if the user is connected to the internet, false otherwise
-     */
-    @JvmStatic
-    @Deprecated("Replaced by extension",
-            ReplaceWith("manager.isConnected", "com.guerinet.suitcase.util.extensions.isConnected"))
-    fun isConnected(manager: ConnectivityManager): Boolean = manager.isConnected
-
-    /**
-     * @param context App context
-     * @return True if the user is connected to the internet, false otherwise
-     */
-    @JvmStatic
-    @Deprecated("Replaced by extension",
-            ReplaceWith("context.isConnected", "com.guerinet.suitcase.util.extensions.isConnected"))
-    fun isConnected(context: Context): Boolean = context.isConnected
-
-    /**
-     * @return Resource Id for a given [attributeId] to set an attribute programmatically, using
-     *  the app [context]
-     */
-    @JvmStatic
-    @Deprecated("Replaced by extension", ReplaceWith("context.getAttributeResourceId(attributeId)",
-            "com.guerinet.suitcase.util.extensions.getAttributeResourceId"))
-    fun getResourceFromAttributes(context: Context, attributeId: Int): Int =
-            context.getAttributeResourceId(attributeId)
-
-    /**
-     * @return The resource Id of the given [type] for the given [id] name in the given [context]
-     */
-    @JvmStatic
-    @Deprecated("Replaced by extension", ReplaceWith("context.getResourceId(type, id)",
-            "com.guerinet.suitcase.util.extensions.getResourceId"))
-    fun getResourceId(context: Context, type: String, id: String): Int =
-            context.getResourceId(type, id)
-
-    /**
-     * Displays a toast of short length using the app [context] and the given [message]
-     */
-    @JvmStatic
-    @Deprecated("Replaced by android-ktx")
-    fun toast(context: Context, @StringRes message: Int) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-    }
-
-    /**
-     * Displays a toast of short length using the app [context] and the given [message]
-     */
-    @JvmStatic
-    @Deprecated("Replaced by android-ktx")
-    fun toast(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-    }
-
-    /**
-     * Opens a given [url] using the app [context]
-     */
-    @JvmStatic
-    @Deprecated("Replaced by extension", ReplaceWith("context.openUrl(url)",
-            "com.guerinet.suitcase.util.extensions.openUrl"))
-    fun openUrl(context: Context, url: String) = context.openUrl(url)
-
-    /**
-     * Opens a Chrome custom tab when opening a [url] using the app [context] with a default share
-     * option. Optionally sets the [toolbarColor] (it's a light grey if none supplied) and uses the
-     * Drawable [closeButtonId] to set a custom close button (it's a black cross if none supplied)
-     */
-    @JvmStatic
-    @Deprecated("Replaced by extension",
-            ReplaceWith("context.openCustomTab(url, toolbarColor, closeButtonId)",
-                    "com.guerinet.suitcase.util.extensions.openCustomTab"))
-    fun openCustomTab(context: Context, url: String, @ColorRes toolbarColor: Int? = null,
-                      @DrawableRes closeButtonId: Int? = null) =
-            context.openCustomTab(url, toolbarColor, closeButtonId)
-
-    /**
-     * Attempts to open the Pdf at the given [path] using the app [context]
-     * @throws ActivityNotFoundException if no app to open the pdf is found
-     */
-    @JvmStatic
-    @Throws(ActivityNotFoundException::class)
-    @Deprecated("Replaced by extension", ReplaceWith("context.openPdf(path)",
-            "com.guerinet.suitcase.util.extensions.openPdf"))
-    fun openPdf(context: Context, path: Uri) = context.openPdf(path)
-
-    /**
-     * Opens an app using the app [context] and the app [packageName] in the Play Store or in the
-     *  browser (if the Play Store is not found)
-     */
-    @JvmStatic
-    @Deprecated("Replaced by extension", ReplaceWith("context.openPlayStoreApp(packageName)",
-            "com.guerinet.suitcase.util.extensions.openPlayStoreApp"))
-    fun openPlayStoreApp(context: Context, packageName: String) =
-            context.openPlayStoreApp(packageName)
 }
