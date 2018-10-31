007144dd8be9ae4bd625476429f27d435c8a03c1
Julien Guerinet <julien@guerinet.com>
2018-10-31 06:52:00 -0400

    Add the UpdateDb and UpdateDao

---
+++ room/src/main/java/UpdateDao.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016-2018 Julien Guerinet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.guerinet.room
+
+import androidx.room.Dao
+import androidx.room.Query
+
+/**
+ * Dao to retrieve the [AppUpdate]s
+ * @author Julien Guerinet
+ * @since 4.3.0
+ */
+@Dao
+interface UpdateDao {
+
+    /**
+     * Returns all of the [AppUpdate]s
+     */
+    @Query("SELECT * FROM AppUpdate ORDER BY id DESC")
+    fun getAll(): List<AppUpdate>
+}
---
+++ room/src/main/java/UpdateDb.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016-2018 Julien Guerinet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.guerinet.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.guerinet.room.converter.ZonedDateTimeConverter
+
+/**
+ * Database to store all [AppUpdate]s
+ * @author Julien Guerinet
+ * @since 4.3.0
+ */
+@Database(exportSchema = false, entities = [AppUpdate::class], version = 1)
+@TypeConverters(ZonedDateTimeConverter::class)
+abstract class UpdateDb : RoomDatabase() {
+
+    abstract fun updateDao(): UpdateDao
+
+    companion object {
+
+        fun init(context: Context): UpdateDb =
+            Room.databaseBuilder(context, UpdateDb::class.java, "update-db").build()
+    }
+}
